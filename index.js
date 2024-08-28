673180f448e4be4bbd012127400254ae1f7cf5f9
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-08-28 21:32:13 +0100

    update

--- index.js
+++ index.js
@@ -1,42 +1,54 @@
-const display = document.querySelector('.display')
+const display = document.querySelector(".display");
 
-const controlBtns = document.querySelector('.controls').children
-const allSymbols = ['+', '-', 'X', '÷', '%', 'C', '=']
+const controlBtns = document.querySelector(".controls").children;
+const allSymbols = ["+", "-", "X", "÷", "%", "C", "="];
 
 //2
-let firstValue = ''
-let secondValue = ''
-let symbol  = ''
+let firstValue = "";
+let secondValue = "";
+let symbol = "";
+let result = "";
+
+//calculate function
+const calculate = () => {
+  firstValue = parseFloat(firstValue);
+  secondValue = parseFloat(secondValue);
+
+  //if statements
+  if (symbol === "+") result = firstValue + secondValue;
+  if (symbol === "-") result = firstValue - secondValue;
+  if (symbol === "X") result = firstValue * secondValue;
+  if (symbol === "÷") result = firstValue / secondValue;
+  if (symbol === "%") result = firstValue % secondValue;
+
+  display.innerText = result;
+  firstValue = result;
+  secondValue = "";
+};
 
 //1
-//array of buttons named controlBtns with a event listener 
-for(let button of controlBtns) {
-    button.addEventListener('click', () => {
-        const { innerText: btnValue } = button
-        
-        //declare a variable 
-        const btnValueIsSymbol = allSymbols.includes(btnValue)
+//array of buttons named controlBtns with a event listener
+for (let button of controlBtns) {
+  button.addEventListener("click", () => {
+    const { innerText: btnValue } = button;
 
-        //if statements true or false 
+    //declare a variable
+    const btnValueIsSymbol = allSymbols.includes(btnValue);
 
-        if(firstValue
-            && btnValueIsSymbol )
-            {
-                symbol = btnValue
-            }
+    //if statements true or false
 
-  // else if statement  - if there's no symbol, that means the user is still inputting first value
-  else if (!symbol) firstValue += btnValue
-   // if there's a symbol, that means the user is done with the first value then add to second
-   else if (symbol) secondValue += btnValue
+    if (firstValue && btnValueIsSymbol) {
+      ///calling calculate function
+      secondValue && calculate();
 
-
-   if(btnValue !== '=')
-
-
-    
-        display.innerText += btnValue
+      symbol = btnValue;
     }
 
-    )
+    // else if statement  - if there's no symbol, that means the user is still inputting first value
+    else if (!symbol) firstValue += btnValue;
+    // if there's a symbol, that means the user is done with the first value then add to second
+    else if (symbol) secondValue += btnValue;
+
+    if (btnValue !== "=") display.innerText += btnValue;
+  });
 }
